87b49d5b955556cc96faca1073cd9afbfd5bc15c
tsuchiyama <tsuchiyama@for-it.co.jp>
2022-04-25 12:05:13 +0900

    Updating Pagination Feature

--- inc/pagination-template.php
+++ inc/pagination-template.php
@@ -1,24 +1,36 @@
+<?php
+
+if (isset($search_item)) {
+    $link .= '&search_item='.$search_item;
+}
+if ($sort != '') {
+    $link .= '&sort='.$sort;
+}
+
+?>
+
 <nav aria-label="Page navigation">
     <ul class="pagination mt-3">
-    <li class='page-item <?php echo ($page_no == 1)? 'disabled' : '' ?>'><a class='page-link' href='?page_no=<?php echo $returnPage;?><?php echo (isset($search_item)) ? '&search_item='.$search_item : '' ?>'>戻る</a></li>
-    <?php
-    $link = '';
-    for ($counter = 1; $counter <= $total_pages; $counter++)
-    {
-        $link = '?page_no='.$counter;
-        if ($counter == $page_no) {
-        echo "<li class='active page-item'><a class='page-link'>$counter</a></li>";	
-        }else{
-            if (isset($search_item)) {
-                $link .= '&search_item='.$search_item;
-            }
-            if ($sort != '') {
-                $link .= '&sort='.$sort;
-            }
-            echo "<li class='page-item'><a class='page-link' href='$link'>$counter</a></li>";
-        }
-    }
+    <li class='page-item <?php echo ($page_no == 1)? 'disabled' : '' ?>'><a class='page-link' href='?page_no=<?php echo $returnPage.$link;?><?php echo (isset($search_item)) ? '&search_item='.$search_item : '' ?>'>戻る</a></li>
+    <?php 
+    if ($page_no == 1): //１だったら１引かれているPageNoを表示しない
+    else:
     ?>
-    <li class='page-item <?php echo ($page_no == $total_pages)? 'disabled' : '' ?>'><a class='page-link' href='?page_no=<?php echo $nextPage;?><?php echo (isset($search_item)) ? '&search_item='.$search_item : '' ?>'>次へ</a></li>
+        <li class='page-item'><a class='page-link' href='?page_no=<?php echo ($page_no -1).$link ;?>'><?php echo $page_no - 1 ?></a></li>
+    <?php endif; ?>
+
+    <li class='page-item active'><a class='page-link' href='?page_no=<?php echo $page_no.$link;?>'><?php echo $page_no?></a></li>
+
+    <!-- TotalPage よりページが小さいときに＋１のページを表示 （例　$totalPage = 6; 6+1=7 ７になっているため、７ページにいくとデータがない)-->
+    <?php if ($total_pages > $page_no) : ?>
+        <li class='page-item'><a class='page-link' href='?page_no=<?php echo ($page_no + 1).$link;?>'><?php echo $page_no + 1 ?></a></li>
+    <?php endif; ?>
+
+    <!-- PageNoが１のとき３ページを表示させるため -->
+    <?php if ($page_no == 1):?>
+        <li class='page-item'><a class='page-link' href='?page_no=<?php echo ($page_no + 2).$link;?>'><?php echo $page_no + 2 ?></a></li>
+    <?php endif; ?>
+
+    <li class='page-item <?php echo ($page_no == $total_pages)? 'disabled' : '' ?>'><a class='page-link' href='?page_no=<?php echo $nextPage.$link;?><?php echo (isset($search_item)) ? '&search_item='.$search_item : '' ?>'>次へ</a></li>
     </ul>
 </nav>
